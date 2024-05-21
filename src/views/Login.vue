b0d40ecbaa642e20f14a7f498bb9e29b4cd1bcf8
Nuo Chen <chenn16@cardiff.ac.uk>
2024-05-21 23:19:20 +0800

    [+] 安装sass

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,24 @@
 <template>
-    <div>
-        login
+    <div class="login">
+        <div class="box"></div>
     </div>
 </template>
 <script setup lang='ts'>
 
 </script>
 <style lang='scss'>
-
+.login{
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(to bottom, #142334, #6894c7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .box{
+        width: 400px;
+        height: 200px;
+        background: #fff;
+        border-radius: 10px;
+    }
+}
 </style>
