97e9eafa856640c45fe6942673007184aae11ab9
Nuo Chen <chenn16@cardiff.ac.uk>
2024-05-22 00:18:51 +0800

    [+] 绘制登录页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,110 @@
 <template>
-    <div class="login">
-        <div class="box"></div>
+  <div class="login">
+    <div class="box">
+      <h2>SM酒店信息管理平台</h2>
+      <el-form
+        size="small"
+        ref="formRef"
+        style="max-width: 600px"
+        :model="formData"
+        status-icon
+        :rules="rules"
+        label-width="40px"
+      >
+        <el-form-item label="账号" prop="loginId">
+          <el-input v-model="formData.loginId" />
+        </el-form-item>
+        <el-form-item label="密码" prop="loginPwd">
+          <el-input v-model="formData.loginPwd" type="password" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm(formRef)">
+            登陆
+          </el-button>
+          <el-button @click="resetForm(formRef)">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
-<script setup lang='ts'>
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import type { FormInstance, FormRules } from "element-plus";
 
-</script>
-<style lang='scss'>
-.login{
-    width: 100vw;
-    height: 100vh;
-    background: linear-gradient(to bottom, #142334, #6894c7);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .box{
-        width: 400px;
-        height: 200px;
-        background: #fff;
-        border-radius: 10px;
+// 定义表单实例
+const formRef = ref<FormInstance>();
+
+// 验证账号
+const validateLoginId = (rule: any, value: any, callback: any) => {
+  if (value === "") {
+    callback(new Error("请输入账号"));
+  } else {
+    callback();
+  }
+};
+
+// 验证密码
+const validatePassword = (rule: any, value: any, callback: any) => {
+  if (value === "") {
+    callback(new Error("请输入密码"));
+  } else {
+    callback();
+  }
+};
+
+// 表单数据
+const formData = reactive({
+  loginId: "",
+  loginPwd: "",
+});
+
+// 验证对象
+const rules = reactive<FormRules<typeof formData>>({
+  loginId: [{ validator: validateLoginId, trigger: "blur" }],
+  loginPwd: [{ validator: validatePassword, trigger: "blur" }],
+});
+
+// 提交表单
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log("submit!");
+    } else {
+      console.log("error submit!");
     }
+  });
+};
+
+// 重置表单
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.resetFields();
+};
+</script>
+
+<style scoped lang="scss">
+.login {
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(to bottom, #142334, #6894c7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box {
+    width: 600px;
+    border-radius: 10px;
+    border: 1px solid #fff;
+    padding: 20px;
+    h2 {
+      color: #fff;
+      font-size: 20px;
+      text-align: center;
+      margin-bottom: 20px;
+    }
+    :deep(.el-form-item__label) {
+      color: #fff;
+    }
+  }
 }
 </style>
