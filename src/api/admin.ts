23877350f2dc8b8af0b6ba4779b760bb668dc0f2
Nuo Chen <chenn16@cardiff.ac.uk>
2024-05-23 21:49:01 +0800

    [+] 添加axios拦截器

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -4,7 +4,11 @@ import { md5 } from "md5js"
 // error ? params: object ? any
 export const $Login = async (params: object | any) => {
     params.password = md5((md5(params.password,32).split('').reverse().join('')),32);
-    let res = await $get("Admin/Login", params);
+    let res = await $get("login", params);
+    if(res.success){
+        // 登陆成功后，将token存储到sessionStorage（浏览器缓存）中
+        sessionStorage.setItem("token", res.token);
+    }
     // let res = await $get("login", params);
     console.log(res);
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -12,6 +12,28 @@ const instance = axios.create({
   timeout: 30000, // Request timeout 30s
 });
 
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+  // 每次发送请求前，向请求头添加token
+  console.log(config)
+  config.headers['token'] = sessionStorage.getItem("token");
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
+
 // get请求方法
 export const $get = async (url: string, params: object = {}) => {
   let { data } = await instance.get(url, { params });
