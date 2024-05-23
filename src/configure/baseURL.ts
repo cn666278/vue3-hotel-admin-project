c8c802c23dd11b16acdf87f18da3b69061a4648d
Nuo Chen <chenn16@cardiff.ac.uk>
2024-05-23 09:59:17 +0800

    [#] 更改项目api地址

--- src/configure/baseURL.ts
+++ src/configure/baseURL.ts
@@ -1,4 +1,4 @@
-export const baseURL_dev: string = "http://127.0.0.1/api/";
+export const baseURL_dev: string = "https://mock.apifox.com/m1/4536901-4184680-default/api";
 export const baseURL_pro: string = "https://bingjs.com:83/";
 export const baseURL_test: string = "https://bingjs.com:83/";
 // export const baseURL: string = process.env.NODE_ENV === "development" ? baseURL_dev : baseURL_pro;
