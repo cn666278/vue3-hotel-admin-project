c17b34c77d16239a06915324503daac34f08b605
Nuo Chen <chenn16@cardiff.ac.uk>
2024-06-04 22:45:34 +0800

    [+] 用户管理：添加用户功能

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -37,3 +37,45 @@ export const $getUserInfo = async (params: object) => {
   console.log(res);
   return res;
 }
+
+// 模拟数据
+let mockData = {
+  count: 1,
+  data: [
+    {
+      id: 1,
+      loginId: "admin",
+      username: "管理员",
+      password: "21232f297a57a5a743894a0e4a801fc3",
+      phone: "10086",
+      email: "123@gmail.com",
+      photo: "",
+      role: { roleId: 1, roleName: "超级管理员" },
+      roleId: 1,
+    },
+    {
+      id: 2,
+      loginId: "user",
+      username: "用户",
+      password: "21232f297a57a5a743894a0e4a801fc3",
+      phone: "10086",
+      email: "helloworld@cf.ac.uk",
+      photo: "",
+      role: { roleId: 2, roleName: "普通用户" },
+      roleId: 2,
+    }
+  ],
+  pageIndex: 1,
+  pageSize: 10,
+};
+
+/**
+ * 获取用户列表
+ * @param：params
+ * @return {{count: number, data: *[], pageIndex: number, pageSize: number}}
+ */
+
+export const $getUserList = async (params: object) => {
+  // let res = await $get("admin/getUserList", params);
+  return mockData;
+}
--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,14 +1,92 @@
 <template>
-    <div class="user">
-      user
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  
-  </script>
-  
-  <style lang="scss">
-  
-  </style>
-  
+  <div class="user">
+    <!-- 用户表格 -->
+    <el-table :data="userList" stripe style="width: 100%">
+      <el-table-column prop="id" label="编号" width="100" />
+      <el-table-column prop="loginId" label="账号" width="150" />
+      <el-table-column prop="username" label="姓名" width="100" />
+      <el-table-column prop="photo" label="头像" width="100" />
+      <el-table-column prop="phone" label="电话" width="200" />
+      <el-table-column prop="email" label="邮箱" width="200" />
+      <el-table-column prop="role.roleName" label="角色" width="150" />
+      <el-table-column label="操作">
+        <template #default="scope">
+          <el-button size="small" @click="handleEdit(scope.row)">
+            编辑
+          </el-button>
+          <el-button
+            size="small"
+            type="danger"
+            @click="handleDelete(scope.row)"
+          >
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { $getUserList } from "../../api/admin.ts";
+import { ElMessageBox } from "element-plus";
+
+// 用户列表
+let userList: any = ref([]);
+
+let total = ref(0);
+
+// 加载用户列表
+const getUserList = async () => {
+  console.log("加载用户列表");
+  let { data, count } = await $getUserList({});
+  userList.value = data;
+  total.value = count;
+  console.log(userList.value);
+  console.log(count);
+};
+
+// 编辑角色
+const handleEdit = async (row: any) => {
+
+};
+
+// 删除角色
+const handleDelete = (row: any) => {
+  ElMessageBox.confirm("确定删除用户 " + row.roleName + " 吗?", "提示", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning",
+  })
+    // .then(async () => {
+    //   // let res = await $deleteRole(row.roleId);
+    //   if (res.code === 200) {
+    //     ElNotification({
+    //       title: "提示",
+    //       message: res.message,
+    //       type: "success",
+    //     });
+    //     // 删除成功后重新加载角色列表
+    //     getRoleList();
+    //     console.log("删除成功");
+    //   } else {
+    //     ElNotification({
+    //       title: "提示",
+    //       message: res.message,
+    //       type: "error",
+    //     });
+    //   }
+    // })
+    // .catch(() => {
+    //   console.log("取消删除");
+    // });
+};
+
+// 页面加载时加载用户列表
+onMounted(() => {
+  getUserList();
+});
+</script>
+
+<style lang="scss"></style>
